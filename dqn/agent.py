1a059a1dd0f80b9d2c72c10f5df548458da9affc
trunghng <trung.skipper@gmail.com>
2022-11-08 20:26:39 +0700

    add double-dqn + reconstruct code

--- dqn/agent.py
+++ dqn/agent.py
@@ -1,9 +1,11 @@
+import gym
 import random
 import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as opt
 
+from abc import ABC, abstractmethod
 from typing import List
 from collections import deque
 import sys
@@ -16,13 +18,33 @@ from network import DeepQNetwork
 from replay_buffer import ReplayBuffer
 
 
-class DQN:
+class Agent(ABC):
+    '''
+    Agent abstract class
+    '''
+
+    @abstractmethod
+    def behave(self, state):
+        pass
+
+
+    @abstractmethod
+    def learn(self):
+        pass
+
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+
+class DQN(Agent):
     '''
     DQN agent class
     '''
 
-    def __init__(self,
-                env,
+    def __init__(self, env_name: str,
                 epsilon_init: float,
                 epsilon_final: float,
                 gamma: float,
@@ -32,20 +54,20 @@ class DQN:
                 update_freq: int,
                 tau: float,
                 n_eps: int,
+                horizon: int,
                 logging_window: int,
                 termination: float,
                 verbose: bool=False,
                 verbose_freq: int=50,
-                model_path: str=None,
-                video_path: str=None,
-                image_path: str=None,
+                save_model: bool=False,
+                render_video: bool=False,
+                save_plot: bool=False,
                 plot: bool=False,
                 seed: int=0):
         '''
         Parameters
         ----------
-        state_size: state size
-        action_size: action size
+        env_name: OpenAI's environment name
         epsilon_init: initial value for exploration param, epsilon, linearly annealing
         epsilon_final: final value for epsilon linearly annealing
         gamma: discount factor
@@ -55,19 +77,20 @@ class DQN:
         update_freq: number of actions seleced by the agent between successive SGD updates
         tau: for Q network parameters' soft update
         n_eps: number of episodes
+        horizon: number of steps allowed
         logging_window: number of episodes to be tracked
         termination: allows algorithm ends when total reward >= @termination
         verbose: whether to display result
         verbose_freq: display every @verbose_freq
-        model_path: model path, for model saving
-        video_path: video path, for saving output as video
-        image_path: image path, for saving plotting result
+        save_model: whether to save the model
+        render_video: whether to render the final simulation and save it
+        save_plot: whether to save the plot
         plot: whether to plot the result
         seed: random seed
         '''
-        state_size =  env.observation_space.shape[0]
-        action_size = env.action_space.n
-        self.env = env
+        self.env = gym.make(env_name)
+        state_size =  self.env.observation_space.shape[0]
+        action_size = self.env.action_space.n
         self.action_space = range(action_size)
         self.epsilon = epsilon_init
         self.epsilon_final = epsilon_final
@@ -79,13 +102,17 @@ class DQN:
         self.update_freq = update_freq
         self.tau = tau
         self.n_eps = n_eps
+        self.horizon = horizon
         self.logging_window = logging_window
         self.termination = termination
         self.verbose = verbose
         self.verbose_freq = verbose_freq
-        self.model_path = model_path
-        self.video_path = video_path
-        self.image_path = image_path
+        self.model_path = f'../models/{self.name()}-{env_name}.pth' \
+            if save_model else None
+        self.video_path = f'../outputs/videos/{self.name()}-{env_name}.mp4' \
+            if render_video else None
+        self.image_path = f'../outputs/images/{self.name()}-{env_name}.png' \
+            if save_plot else None
         self.plot = plot
         random.seed(seed)
         self.env.seed(seed)
@@ -97,7 +124,11 @@ class DQN:
         self.optimizer = opt.Adam(self.Qnet.parameters(), lr=lr)
 
 
-    def epsilon_annealing(self, epsilon_decay: float) -> None:
+    def name(self) -> str:
+        return type(self).__name__
+
+
+    def anneal_epsilon(self, epsilon_decay: float) -> None:
         '''
         Epsilon linearly annealing
 
@@ -146,6 +177,13 @@ class DQN:
         return action
 
 
+    def get_target(self, next_states, rewards, terminated):
+        q_targets_next = torch.max(self.Qnet_target(next_states), dim=1)[0].unsqueeze(1)
+        q_targets = rewards + self.gamma * q_targets_next * (1 - terminated)
+
+        return q_targets
+
+
     def learn(self) -> None:
         if self.current_step % self.update_freq == 0 \
                 and len(self.buffer) >= self.batch_size:
@@ -156,8 +194,7 @@ class DQN:
             next_states = torch.from_numpy(np.vstack(next_states)).float().to(self.device)
             terminated = torch.from_numpy(np.vstack(terminated).astype(np.uint8)).float().to(self.device)
 
-            q_targets_next = torch.max(self.Qnet_target(next_states), dim=1)[0].unsqueeze(1)
-            q_targets = rewards + self.gamma * q_targets_next * (1 - terminated)
+            q_targets = self.get_target(next_states, rewards, terminated)
             q_expected = self.Qnet(states).gather(1, actions)
             
             loss = F.mse_loss(q_expected, q_targets)
@@ -185,16 +222,17 @@ class DQN:
         torch.save(self.Qnet.state_dict(), self.model_path)
 
 
-    def run(self):
-        epsilon_decay = (self.epsilon - self.epsilon_final) / (self.n_eps / 2)
+    def run(self) -> List[float]:
         total_reward_list = []
         total_reward_history = deque(maxlen=self.logging_window)
+        epsilon_decay = (self.epsilon - self.epsilon_final) / (self.n_eps / 2)
 
         for ep in range(1, self.n_eps + 1):
             state = self.env.reset()
             total_reward = 0
+            step = 0
 
-            while True:
+            while step < self.horizon:
                 action = self.behave(state)
                 next_state, reward, terminated, _ = self.env.step(action)
                 total_reward += reward
@@ -205,10 +243,11 @@ class DQN:
                 self.store_transition(state, action, reward, next_state, terminated)
                 self.learn()
                 state = next_state
+                step += 1
 
             total_reward_list.append(total_reward)
             total_reward_history.append(total_reward)
-            self.epsilon_annealing(epsilon_decay)
+            self.anneal_epsilon(epsilon_decay)
 
             if self.verbose and ep % self.verbose_freq == 0:
                 print(f'Ep {ep}, average total reward {np.mean(total_reward_history):.2f}')
@@ -223,3 +262,46 @@ class DQN:
 
         if self.plot:
             plot.total_reward(total_reward_list, self.image_path)
+
+        return total_reward_list
+
+
+
+class DoubleDQN(DQN):
+    '''
+    Double DQN agent class
+    '''
+
+    def __init__(self, env_name: str,
+                epsilon_init: float,
+                epsilon_final: float,
+                gamma: float,
+                lr: float,
+                buffer_size: int,
+                batch_size: int,
+                update_freq: int,
+                tau: float,
+                n_eps: int,
+                horizon: int,
+                logging_window: int,
+                termination: float,
+                verbose: bool=False,
+                verbose_freq: int=50,
+                save_model: bool=False,
+                render_video: bool=False,
+                save_plot: bool=False,
+                plot: bool=False,
+                seed: int=0):
+        super().__init__(env_name, epsilon_init, epsilon_final, gamma, lr,
+                        buffer_size, batch_size, update_freq, tau,
+                        n_eps, horizon, logging_window, termination,
+                        verbose, verbose_freq, save_model, render_video,
+                        save_plot, plot, seed)
+
+
+    def get_target(self, next_states, rewards, terminated):
+        max_actions = torch.argmax(self.Qnet(next_states), dim=1).unsqueeze(1)
+        q_targets_max_actions = self.Qnet_target(next_states).gather(1, max_actions)
+        q_targets = rewards + self.gamma * q_targets_max_actions * (1 - terminated)
+
+        return q_targets
