6e1f621351396a465cff9c2440de2a0098179f44
trunghng <trung.skipper@gmail.com>
2022-12-05 10:37:47 +0700

    refactor vpg

--- zoo/pg/bash_scripts/CartPole-v0.sh
+++ zoo/pg/bash_scripts/CartPole-v0.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cd ..
-python3 main.py --env CartPole-v0 --seed 1 --pi-lr 7e-4 --v-lr 1e-3 \
-	--epochs 500 --steps-per-epoch 2500 --train-v-iters 20 \
-	--max-ep-len 300 --gamma 0.99 --lamb 0.96 --goal 195 --save --render
-python3 main.py --eval --model-path ./output/models/CartPole-v0 --env CartPole-v0
+python3 main.py --env CartPole-v0 --seed 1 --pi-lr 8e-3 --v-lr 3e-3 \
+	--epochs 50 --steps-per-epoch 5000 --train-v-iters 20 \
+	--max-ep-len 500 --gamma 0.98 --lamb 0.96 --goal 200 --save --render
+python3 main.py --eval --model-path ./output/models/VPG-CartPole-v0.pth --env CartPole-v0
